abd56a4482edb47d32d1d761a073db1465cba9c5
Thushani19 <thushanirameshwaran@gmail.com>
2025-04-06 17:21:44 +0530

    Add files via upload

---
+++ Day2/Question1/App.java
@@ -0,0 +1,17 @@
+import it.*;
+import it.hons.*;
+import amc.*;
+
+class App{
+	public static void main(String[] ar){
+		it.FirstYear sy=new it.FirstYear();
+		amc.FirstYear syi=new amc.FirstYear();
+		SecondYear si=new SecondYear();
+		ForthYear itHons=new ForthYear();
+		
+		sy.display();
+		syi.display();
+		si.display();
+		itHons.display();
+	}
+}
